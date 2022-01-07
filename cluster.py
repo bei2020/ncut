862bc41588f7ee260394d129bc2c642347bea49e
bei2020 <beibao@hotmail.com>
2022-01-07 13:35:35 +0800

    wt

--- cluster.py
+++ cluster.py
@@ -19,8 +19,8 @@ def msimg(img, ssig=1, rsig=None, mcont=5, init_wt=1):
         nint = 2
         # 1234:ESWN, Io: DHWC, D=8 directions
         # curvature
-        grad_E = -pim[1:1 + I, 1:-1, :] + pim[1:1 + I, 2:, :]
-        grad_S = -pim[1:-1, 1:1 + J, :] + pim[2:, 1:1 + J, :]
+        grad_E = pim[1:1 + I, 2:, :]-pim[1:1 + I, 1:-1, :]
+        grad_S = pim[2:, 1:1 + J, :]-pim[1:-1, 1:1 + J, :]
         grad_SE = pim[2:, 2:, :] - pim[1:-1, 1:-1, :]
         grad_NE = pim[:I, 2:, :] - pim[1:-1, 1:-1,:]
         if not rsig:
@@ -37,23 +37,11 @@ def msimg(img, ssig=1, rsig=None, mcont=5, init_wt=1):
         w_NE = edge_weight(grad_NE, rsig)
         w_NE[:,-1]=0
         w_NE[0,:]=0
-        w_W = np.hstack((np.zeros((I, 1)), w_E[:, :-1]))
-        w_N = np.vstack((np.zeros((1, J)), w_S[:-1, :]))
-        w_NW = np.hstack((np.zeros((I, 1)),np.vstack((np.zeros((1, J - 1)), w_SE[:-1,:-1]))))
-        w_SW = np.hstack((np.zeros((I, 1)), np.vstack((w_NE[1:,:-1], np.zeros((1, J - 1))))))
-        wn=np.sum([w_E,w_S,w_SE,w_NE,w_W,w_N,w_NW,w_SW])
+        wn=2*np.sum([w_E,w_S,w_SE,w_NE])
         w_E/=wn
         w_S/=wn
         w_SE/=wn
         w_NE/=wn
-        w_W/=wn
-        w_N/=wn
-        w_NW/=wn
-        w_SW/=wn
-        di=np.sum(np.stack((w_E,w_S,w_SE,w_NE,w_W,w_N,w_NW,w_SW),0),0)
-        Io = (np.multiply(w_E.reshape(I,J,1), pim[1:-1, 2:, :]) + np.multiply(w_S.reshape(I,J,1), pim[2:, 1:-1, :]) + np.multiply(w_W.reshape(I,J,1), pim[1:-1, :J, :]) \
-              + np.multiply( w_N.reshape(I,J,1), pim[:I, 1:-1, :]) + np.multiply(w_SE.reshape(I,J,1), pim[2:, 2:, :]) + np.multiply(w_NE.reshape(I,J,1), pim[:I, 2:, :]) \
-              + np.multiply( w_NW.reshape(I,J,1), pim[:I, :J, :]) + np.multiply(w_SW.reshape(I,J,1), pim[2:, :J, :]))
         # fn = 'wt_rsig%f_%s' % (rsig, fn)
         # with open('%s.npy' % fn, 'wb') as f:
         #     np.save(f, np.stack((w_E,w_S,w_SE,w_NE)))
@@ -64,24 +52,21 @@ def msimg(img, ssig=1, rsig=None, mcont=5, init_wt=1):
         with open('%s.npy' % fn, 'rb') as f:
             wt = np.load(f)
         w_E,w_S,w_SE,w_NE=wt
-        w_W = np.hstack((np.zeros((I, 1)), w_E[:, :-1]))
-        w_N = np.vstack((np.zeros((1, J)), w_S[:-1, :,]))
-        w_NW = np.hstack((np.zeros((I, 1)),np.vstack((np.zeros((1, J - 1)), w_SE[:-1,:-1]))))
-        w_SW = np.hstack((np.zeros((I, 1)), np.vstack((w_NE[1:,:-1], np.zeros((1, J - 1))))))
 
     def msiter(niter=1000):
-        nonlocal pim, v, w_E,w_S,w_SE,w_NE,w_W,w_N,w_NW,w_SW
-        # wt[:,gloc[0],gloc[1]]=0
-        # wt[:,floc[0],floc[1]]=0
+        nonlocal pim, v, w_E,w_S,w_SE,w_NE
         for i in range(niter):
-            Io = (np.multiply(w_E.reshape(I,J,1), pim[1:-1, 2:, :]+ gamma * v[1:-1, 2:, :]) + np.multiply(w_S.reshape(I,J,1), pim[2:, 1:-1, :]+ gamma * v[2:, 1:-1, :]) + np.multiply(w_W.reshape(I,J,1), pim[1:-1, :J, :]+ gamma * v[1:-1, :J, :]) \
-                  + np.multiply( w_N.reshape(I,J,1), pim[:I, 1:-1, :]+ gamma * v[:I, 1:-1, :]) + np.multiply(w_SE.reshape(I,J,1), pim[2:, 2:, :]+ gamma * v[2:, 2:, :]) + np.multiply(w_NE.reshape(I,J,1), pim[:I, 2:, :]+ gamma * v[:I, 2:, :]) \
-                  + np.multiply( w_NW.reshape(I,J,1), pim[:I, :J, :]+ gamma * v[:I, :J, :]) + np.multiply(w_SW.reshape(I,J,1), pim[2:, :J, :]+ gamma * v[2:, :J, :]))
-            # gp = Io - np.multiply((pim+ gamma * v)[1:-1,1:-1,:],di.reshape((I, J, 1)))
-            gp = Io - np.multiply((pim+ gamma * v)[1:-1,1:-1,:],di.reshape((I, J, 1)))
-            v[1:-1,1:-1,:] = gamma * v[1:-1,1:-1,:] + a * gp
+            m_E = np.multiply(w_E.reshape(I,J,1), pim[1:-1, 2:, :]-pim[1:-1, 1:-1, :] + gamma * v[1:-1, 2:, :]-gamma * v[1:-1,1:-1,:])
+            m_S = np.multiply(w_S.reshape(I,J,1), pim[2:, 1:-1, :]-pim[1:-1, 1:-1, :] + gamma * v[2:, 1:-1, :]-gamma * v[1:-1,1:-1,:])
+            m_SE = np.multiply(w_SE.reshape(I,J,1),pim[2:, 2:, :] - pim[1:-1, 1:-1, :]+ gamma * v[2:, 2:, :]-gamma * v[1:-1,1:-1,:])
+            m_NE = np.multiply(w_NE.reshape(I,J,1),pim[:-2, 2:, :] - pim[1:-1, 1:-1,:]+ gamma * v[:-2, 2:, :]-gamma * v[1:-1,1:-1,:])
+            Io=m_E+m_S+m_SE+m_NE
+            Io[:,1:,:]-=m_E[:,:-1,:]
+            Io[1:,:,:]-=m_S[:-1,:,:]
+            Io[1:,1:,:]-=m_SE[:-1,:-1,:]
+            Io[:-1,1:,:]-=m_NE[1:,:-1,:]
+            v[1:-1,1:-1,:] = gamma * v[1:-1,1:-1,:] + a * Io
             pim += v
-            # pim[1:-1,1:-1,:] += gp
         return
 
     def msconti(ncont=0, mcont=12):
